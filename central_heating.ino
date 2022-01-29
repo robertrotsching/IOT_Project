67daa02fea132f574d365196dd6dbecf7ca2c7cc
robertrotsching <robert.rotsching@stud.acs.upb.ro>
2022-01-29 13:07:16 +0200

    Add files via upload

---
+++ central_heating.ino
@@ -0,0 +1,187 @@
+// Load Wi-Fi library
+#include <WiFi.h>
+
+hw_timer_t * timer = NULL;
+
+int led = 15;
+int termistor = 35;
+int temperature_low_limit = 23;
+int temperature_high_limit = 30;
+volatile byte state = LOW;
+bool control_remote = false;
+
+// Replace with your network credentials
+const char* ssid = "ssid";
+const char* password = "password";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output26State = "off";
+
+// Assign output variables to GPIO pins
+const int output26 = 26;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void IRAM_ATTR onTimer(){
+  if (!state && (get_temperature() < temperature_low_limit) ){
+      if (!control_remote){
+        state = HIGH;
+      }
+  }
+  if (state && (get_temperature() > temperature_high_limit)){
+    if (!control_remote){
+      state = LOW;
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(led, OUTPUT);
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmEnable(timer);
+  // Initialize the output variables as outputs
+  pinMode(output26, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output26, LOW);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  if (state){
+    start_central_heating();
+  }
+  else{
+    stop_central_heating();
+  }
+  
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the Serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the Serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /26/on") >= 0) {
+              Serial.println("GPIO 26 on");
+              output26State = "on";
+              //digitalWrite(output26, HIGH);
+              state = HIGH;
+              control_remote = true;
+            } else if (header.indexOf("GET /26/off") >= 0) {
+              Serial.println("GPIO 26 off");
+              output26State = "off";
+              //digitalWrite(output26, LOW);
+              state = LOW;
+              control_remote = false;
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>Home Automation System</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>Central Heating " + output26State + "</p>");
+            // If the output26State is off, it displays the ON button       
+            if (output26State=="off") {
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
+
+void start_central_heating(){
+  state = HIGH;
+  digitalWrite(led, HIGH);
+  digitalWrite(output26, HIGH);
+}
+
+void stop_central_heating(){
+  state = LOW;
+  digitalWrite(led, LOW);
+  digitalWrite(output26, LOW);
+}
+
+int get_temperature(){
+  int x = 0;
+  x = (analogRead(termistor) * 26) / 2154;
+  Serial.println(x);
+  return x;
+}
